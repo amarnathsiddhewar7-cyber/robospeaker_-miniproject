17bf9a7900a530f758d9bee62287e0c46ab32804
amarnathsiddhewar7-cyber <amarnathsiddhewar7@gmail.com>
2026-03-14 19:27:34 +0530

    Add files via upload

---
+++ robospeaker miniproject.py
@@ -0,0 +1,12 @@
+import os         # os module in Python is used to interact with the operating system.
+
+
+while True:
+    text = input("Enter text (type 'stop' to exit): ")
+    
+    if text == "stop":
+        print("Robo Speaker stopped")
+        break
+    
+    # Windows Text-to-Speech using PowerShell
+    os.system(f'powershell -Command "Add-Type –AssemblyName System.speech; $speak = New-Object System.Speech.Synthesis.SpeechSynthesizer; $speak.Speak(\'{text}\');"')
